e88a33695b210ffe5912df294277d7e63d903bfe
5LICK <slicknslick@yahoo.com>
2015-11-19 18:06:35 +0200

    admob interstitial ad

--- android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
+++ android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
@@ -1,17 +1,20 @@
 package com.dragit.slickstars.game.android;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dragit.slickstars.game.MainGame;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class AndroidLauncher extends AndroidApplication {
 	
@@ -25,38 +28,47 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useCompass = false;
 		config.useAccelerometer = false;
 		initialize(new MainGame(), config);
-		
-		
         
-      //создём главный слой	
-        RelativeLayout layout = new RelativeLayout(this);
-        //устанавливаем флаги, которые устанавливались в методе initialize() вместо нас
+        /*RelativeLayout layout = new RelativeLayout(this);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-                       WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);*/
 
-        //представление для LibGDX
         View gameView = initializeForView(new MainGame(), config);
         
-        //представление и настройка AdMob
-        
-        adView = (AdView) findViewById(R.id.adView);
+        /*RelativeLayout layout = R.layout.activity_main;
+        RelativeLayout.LayoutParams adParams =
+        new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT,
+        RelativeLayout.LayoutParams.WRAP_CONTENT);
+        adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);*/
+
+        setContentView(R.layout.activity_main);
+        InterstitialAd iad = new InterstitialAd(this);
+        iad.setAdUnitId(getString(R.string.banner_ad_unit_id));
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("0123456789ABCDEF")
+                .build();
+
+        iad.loadAd(adRequest);
+
+        /*adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
+        adView.loadAd(adRequest);*/
 
-         //добавление представление игрык слою
-        layout.addView(gameView);
+        if (iad.isLoaded()) {
+            iad.show();
+        }
+        else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+        }
 
-        RelativeLayout.LayoutParams adParams = 
-                      new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT, 
-                              RelativeLayout.LayoutParams.WRAP_CONTENT);
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM); 
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT); 
-        //добавление представление рекламы к слою
-        layout.addView(adView, adParams);  
-            
-        //всё соединяем в одной слое
-        setContentView(layout); 
-	}
+        RelativeLayout gamelLayout = (RelativeLayout) findViewById(R.id.gameRelative);
+        gamelLayout.addView(gameView);
+
+
+
+    }
 }
