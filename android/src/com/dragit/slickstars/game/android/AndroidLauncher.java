b148cb786b37b72d295f49f3296f445110e05a14
5LICK <slicknslick@yahoo.com>
2015-11-20 14:03:23 +0200

    admob

--- android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
+++ android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dragit.slickstars.game.MainGame;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
@@ -19,56 +20,27 @@ import com.google.android.gms.ads.InterstitialAd;
 public class AndroidLauncher extends AndroidApplication {
 	
 	protected AdView adView;
-	
+	private InterstitialAd iad;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.hideStatusBar = true;
 		config.useCompass = false;
 		config.useAccelerometer = false;
 		initialize(new MainGame(), config);
-        
-        /*RelativeLayout layout = new RelativeLayout(this);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().clearFlags( WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);*/
 
-        View gameView = initializeForView(new MainGame(), config);
-        
-        /*RelativeLayout layout = R.layout.activity_main;
-        RelativeLayout.LayoutParams adParams =
-        new RelativeLayout.LayoutParams( RelativeLayout.LayoutParams.WRAP_CONTENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT);
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);*/
-
-        setContentView(R.layout.activity_main);
-        InterstitialAd iad = new InterstitialAd(this);
+        iad = new InterstitialAd(this);
         iad.setAdUnitId(getString(R.string.banner_ad_unit_id));
-
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice("0123456789ABCDEF")
-                .build();
-
+        AdRequest adRequest = new AdRequest.Builder().build();
         iad.loadAd(adRequest);
 
-        /*adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);*/
-
-        if (iad.isLoaded()) {
-            iad.show();
-        }
-        else {
-            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
-        }
-
-        RelativeLayout gamelLayout = (RelativeLayout) findViewById(R.id.gameRelative);
-        gamelLayout.addView(gameView);
-
-
-
+        iad.setAdListener(new AdListener(){
+            public void onAdLoaded(){
+                iad.show();
+            }
+        });
     }
 }
