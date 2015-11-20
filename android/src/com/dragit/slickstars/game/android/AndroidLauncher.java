f7f03c636fcfe15d395e4f5b9e9cfaef47c87881
5LICK <slicknslick@yahoo.com>
2015-11-20 14:38:34 +0200

    deleted unused files

--- android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
+++ android/src/com/dragit/slickstars/game/android/AndroidLauncher.java
@@ -1,26 +1,16 @@
 package com.dragit.slickstars.game.android;
 
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dragit.slickstars.game.MainGame;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 
 public class AndroidLauncher extends AndroidApplication {
 	
 	protected AdView adView;
-	private InterstitialAd iad;
+	//private InterstitialAd iad;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
@@ -32,7 +22,7 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
 		initialize(new MainGame(), config);
 
-        iad = new InterstitialAd(this);
+        /*iad = new InterstitialAd(this);
         iad.setAdUnitId(getString(R.string.banner_ad_unit_id));
         AdRequest adRequest = new AdRequest.Builder()
 				.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
@@ -43,6 +33,6 @@ public class AndroidLauncher extends AndroidApplication {
             public void onAdLoaded(){
                 iad.show();
             }
-        });
+        });*/
     }
 }
